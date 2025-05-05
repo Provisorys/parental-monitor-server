f77d423c4232ba18f91f9a27ae5318bc48acf2b0
Provisorys <provisory.y@gmail.com>
2025-05-05 08:39:26 -0300

    Adicionar log de depuração na rota /

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+    console.log('Requisição GET / recebida'); // Log para depuração
     res.status(200).send('Servidor Parental Monitor rodando!');
 });
 
